88de4d2b86d60daffc0432b75aea2bbd6a111cd7
JohnPoliakov <punishercinq@gmail.com>
2022-09-26 14:38:06 +0200

    menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    
+
+}
