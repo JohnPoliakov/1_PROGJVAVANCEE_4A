25ec271bc5b0a251a17a7ddb82b6bbf2a883d23d
LAPTOP-RCLTKRTD\lucas <lucasmc77700@gmail.com>
2022-09-26 19:03:49 +0200

    avancement explosion

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,7 +9,8 @@ public class PlayerScript : MonoBehaviour
 
     public float speed = 10f;
     public Vector3 currentPosition;
-    public Vector3 currentPlaceOccuped;
+    public GameObject currentPlaceOccuped;
+    public GameObject prefabTest;
     
     // Start is called before the first frame update
     void Start()
@@ -25,9 +26,26 @@ public class PlayerScript : MonoBehaviour
         rb.velocity = new Vector3(xMove, 0, yMove) * speed;
 
         currentPosition = gameObject.transform.position;
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameObject objInstantiate = Instantiate(prefabTest, currentPlaceOccuped.transform.GetChild(0));
+            List<GameObject> explosionBlock = new List<GameObject>();
+            Vector3 positionExplosionRight = objInstantiate.transform.position + new Vector3(1,0);
+            Destroy(objInstantiate, 3);
+        }
     }
     
     
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Grass")
+        {
+            currentPlaceOccuped = collision.gameObject; 
+        }
+        
+    }
 
     private void FixedUpdate()
     {
